b7570bb8234ca3d3e125e0d294bfb8d629163cc8
Brian Zimmer <bzimmer@ziclix.com>
2024-08-17 21:28:59 +0200

    improved error handling; corrected formatting

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 A simple tool for programatically trashing selected files in MacOS Finder.
 
-Supports files passed as arguments or from standard input, either as absolute or relative paths.
+Supports files passed as arguments or from standard input, either as absolute or
+relative paths.
 
 ## Installation
 
@@ -26,7 +27,10 @@ Compile and install:
 make install
 ```
 
-Now you should be able to use `trash` from anywhere in your terminal.  By default, the binary and manpage will be installed under `/usr/local`.  You can adjust locations using the variables in the Makefile, e.g. `make install PREFIX=/opt`.
+Now you should be able to use `trash` from anywhere in your terminal.  By
+default, the binary and manpage will be installed under `/usr/local`.  You can
+adjust locations using the variables in the Makefile, e.g. `make install
+PREFIX=/opt`.
 
 ## Usage
 
@@ -36,29 +40,48 @@ trash [-0] [--] [files...]
   --: End of options processing; all subsequent arguments are files
 ```
 
-Either absolute and relative paths may be supplied, and may be mixed in the same invocation.  Relative paths will be resolved from the current working directory.
+Either absolute and relative paths may be supplied, and may be mixed in the same
+invocation.  Relative paths will be resolved from the current working directory.
 
-Paths may be passed as arguments, or from standard input, but not both.  If supplied via arguments whilst stdin has data available, then stdin will be ignored and the arguments will be used, and a warning will be issued.  This warning is suppressed by the `--` option.
+Paths may be passed as arguments, or from standard input, but not both.  If
+supplied via arguments whilst stdin has data available, then stdin will be
+ignored and the arguments will be used, and a warning will be issued.  This
+warning is suppressed by the `--` option.
 
-Always supply the `--` option when passing filename(s) via arguments from within another script/application.  Not only does it silence the warning, this prevents misbehaviour when a relative path begins with a hyphen.
+Always supply the `--` option when passing filename(s) via arguments from within
+another script/application.  Not only does it silence the warning, this prevents
+misbehaviour when a relative path begins with a hyphen.
 
 ## Examples
 
-1. Trash files passed as arguments:
+### Trash files passed as arguments:
 
 ```sh
 trash file1.txt file2.txt
 ```
 
-2. Find and trash files with a PDF extension in your Documents folder and subfolders:
+### Find and trash files with a PDF extension in your Documents folder and subfolders:
 
 ```sh
-find ~/Documents -iname '*.pdf' -print0 | trash -0
+fd -0 -e pdf . ~/Documents | trash -0
 ```
 
-3. Trash a file supplied from user input:
+Alternatively:
+
+```sh
+fd -e pdf . ~/Documents -X trash {}
+```
+
+### Trash a file supplied from user input:
 
 ```sh
 read -p 'file: ' FILENAME
 trash -- "$FILENAME"
 ```
+
+## Credits
+
+Shamelessly borrowed the concept and much of the code from
+[reveal](https://github.com/inopinatus/reveal), a tool I used nearly daily. The
+primary changes are reformatting and the action of revealing files was changed
+to trashing them.
--- trash.swift
+++ trash.swift
@@ -3,48 +3,57 @@
 import AppKit
 import Foundation
 
+enum ExitCode: Int32 {
+  case success
+  case fileDoesNotExist
+  case unknownError
+}
+
 let progname = URL(fileURLWithPath: CommandLine.arguments[0]).lastPathComponent
 
 func warn(_ message: String, when: Bool = true) {
-    guard when else { return }
-    fputs("\(message)\n", stderr)
+  guard when else { return }
+  fputs("\(message)\n", stderr)
 }
 
 func warnx(_ message: String, when: Bool = true) {
-    warn("\(progname): \(message)", when: when)
+  warn("\(progname): \(message)", when: when)
 }
 
 func usage() {
-    warn("usage: \(progname) [-0] [--] [files...]")
-    warn("  -0: Expect NUL ('\\0')-terminated filename strings on stdin")
-    warn("  --: End of options processing; all subsequent arguments are files")
-    exit(1)
+  warn("usage: \(progname) [-0] [--] [files...]")
+  warn("  -0: Expect NUL ('\\0')-terminated filename strings on stdin")
+  warn("  --: End of options processing; all subsequent arguments are files")
+  exit(1)
 }
 
 func isInputAvailable() -> Bool {
-    var pollFD = pollfd(fd: STDIN_FILENO, events: Int16(POLLIN), revents: 0)
-    return poll(&pollFD, 1, 0) > 0 && (pollFD.revents & Int16(POLLIN)) != 0
+  var pollFD = pollfd(fd: STDIN_FILENO, events: Int16(POLLIN), revents: 0)
+  return poll(&pollFD, 1, 0) > 0 && (pollFD.revents & Int16(POLLIN)) != 0
 }
 
 struct InputSplitter: IteratorProtocol {
-    enum Separator: UInt8 { case NUL = 0, LF = 10 }
-    let separator: Separator
-    var buffer = Data(capacity: Int(PATH_MAX))
+  enum Separator: UInt8 {
+    case null = 0
+    case lineFeed = 10
+  }
+  let separator: Separator
+  var buffer = Data(capacity: Int(PATH_MAX))
 
-    mutating func result() -> String? {
-        defer { buffer.removeAll(keepingCapacity: true) }
-        return buffer.isEmpty ? nil : String(data: buffer, encoding: .utf8)
-    }
-    mutating func next() -> String? {
-        var char: UInt8 = 0
-        while read(STDIN_FILENO, &char, 1) == 1 {
-            if char == separator.rawValue {
-                return result()
-            }
-            buffer.append(char)
-        }
+  mutating func result() -> String? {
+    defer { buffer.removeAll(keepingCapacity: true) }
+    return buffer.isEmpty ? nil : String(data: buffer, encoding: .utf8)
+  }
+  mutating func next() -> String? {
+    var char: UInt8 = 0
+    while read(STDIN_FILENO, &char, 1) == 1 {
+      if char == separator.rawValue {
         return result()
+      }
+      buffer.append(char)
     }
+    return result()
+  }
 }
 
 var nullSeparated = false
@@ -52,31 +61,42 @@ var fileArguments = false
 
 var args = CommandLine.arguments.dropFirst()
 loop: while let arg = args.first, arg.starts(with: "-") {
-    args = args.dropFirst()
+  args = args.dropFirst()
 
-    switch arg {
-    case "-0":
-        nullSeparated = true
-    case "--":
-        fileArguments = true
-        break loop
-    default:
-        warnx("invalid option: \(arg)")
-        usage()
-    }
+  switch arg {
+  case "-0":
+    nullSeparated = true
+  case "--":
+    fileArguments = true
+    break loop
+  default:
+    warnx("invalid option: \(arg)")
+    usage()
+  }
 }
 
 if !(fileArguments || args.isEmpty) {
-    fileArguments = true
-    warnx("ignoring input on stdin due to file arguments", when: isInputAvailable())
+  fileArguments = true
+  warnx("ignoring input on stdin due to file arguments", when: isInputAvailable())
 }
 warnx("ignoring -0 option due to file arguments", when: fileArguments && nullSeparated)
 
-let urls = (fileArguments
-            ? AnySequence(args)
-            : AnySequence { InputSplitter(separator: nullSeparated ? .NUL : .LF) })
-              .compactMap { URL(fileURLWithPath: $0).absoluteURL }
+let urls =
+  (fileArguments
+  ? AnySequence(args)
+  : AnySequence { InputSplitter(separator: nullSeparated ? .null : .lineFeed) })
+  .compactMap { URL(fileURLWithPath: $0).absoluteURL }
 
-for url in urls {
-    try! FileManager.default.trashItem(at: url, resultingItemURL: nil)
+var exitCode = ExitCode.success
+for url in urls where exitCode.rawValue < ExitCode.unknownError.rawValue {
+  do {
+    try FileManager.default.trashItem(at: url, resultingItemURL: nil)
+  } catch CocoaError.fileNoSuchFile {
+    exitCode = ExitCode.fileDoesNotExist
+    warn("invalid path: \(url.path)")
+  } catch {
+    exitCode = ExitCode.unknownError
+    warn("unexpected error: \(error)")
+  }
 }
+exit(exitCode.rawValue)
